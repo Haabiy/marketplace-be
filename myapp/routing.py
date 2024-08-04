8fe4d44958d306828247334c18ad544b95907434
Haabiy <abiy.dema@gmail.com>
2024-08-04 23:33:16 +0200

    added FilterDataSourcesConsumer

--- myapp/routing.py
+++ myapp/routing.py
@@ -4,6 +4,7 @@ from .consumers import *
 websocket_urlpatterns = [
     re_path(r'ws/data_library/$', DataLibraryConsumer.as_asgi()),
     re_path(r'ws/read_source/', DataSourcesConsumer.as_asgi()),
+    re_path(r'ws/filter_source/', FilterDataSourcesConsumer.as_asgi()),
     re_path(r'ws/source/', SourceConsumer.as_asgi()), 
     re_path(r'ws/data_source_activation/', ActivationSourcesConsumer.as_asgi()), 
     re_path(r'ws/EMRConsumer/', EMRConsumer.as_asgi()), 
